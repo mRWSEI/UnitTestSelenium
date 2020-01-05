45243e462596ebd677b9870d4e951725fe4bf162
MR <m.rzechowicz@gmail.com>
2020-01-05 01:19:28 +0100

    dokończenie prac

---
+++ SeleniumTest_01/SeleniumClass/AdminPanel.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumClass
+{
+    public class AdminPanel
+    {   
+        private readonly IWebDriver driver;
+        public AdminPanel(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        #region Menu
+        [FindsBy(How = How.XPath, Using = @"/html/body/div[5]/ul/li[1]/a/span")]
+        public IWebElement MenuAdmin { get; set; }
+
+        [FindsBy(How = How.XPath, Using = @"/html/body/div[5]/ul/li[1]/ul/li[3]/a/span")]
+        public IWebElement MenuAdminQualification  { get; set; }
+
+        [FindsBy(How = How.XPath, Using = @"/html/body/div[5]/ul/li[1]/ul/li[3]/ul/li[1]/a/span")]
+        public IWebElement MenuAdminQualificationSkills { get; set; }
+
+        #endregion
+
+        [FindsBy(How = How.Id, Using = "btnAdd")]
+        public IWebElement BtnAdd { get; set; }
+
+        [FindsBy(How = How.Id, Using = "btnSave")]
+        public IWebElement BtnSave { get; set; }
+
+        [FindsBy(How = How.Id, Using = "messageBalloon_success")]
+        public IWebElement MessageBallonSuccess { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = @"/html/body/div[5]/ul/li[1]/ul/li[3]/ul/li[1]/a")]
+        public IWebElement MenuSkills { get; set; }
+
+        [FindsBy(How = How.Id, Using = "skill_name")]
+        public IWebElement InputSkillName { get; set; }
+
+
+        [FindsBy(How = How.Id, Using = "skill_description")]
+        public IWebElement InputSkillDescription { get; set; }
+    }
+}
--- SeleniumTest_01/SeleniumClass/Program.cs
+++ SeleniumTest_01/SeleniumClass/Program.cs
@@ -8,6 +8,7 @@ using SeleniumExtras.PageObjects;
 using OnlineStore.PageObjects;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 
 namespace SeleniumClass
 {
@@ -16,37 +17,79 @@ namespace SeleniumClass
         public static void Main()
         {
 
-            FirefoxDriver _driver = new FirefoxDriver();
-            //_driver.SwitchTo().DefaultContent();
-            //_driver.Switch().Frame().
-
-            LoginPage loginPage = new LoginPage(_driver);
-            loginPage.GoToPage();
-
-
-            //_driver.SwitchTo().Frame("preview-frame");
-
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-            //check if there is any iframe then get that frame and switch on frame using folloiwng code.
-            var frame = wait.Until(ExpectedConditions.ElementExists(By.Name("preview-frame")));
-            _driver.SwitchTo().Frame(frame);
+            using (FirefoxDriver _driver = new FirefoxDriver())
+            {
+                try
+                {
+                    //_driver.SwitchTo().DefaultContent();
+                    //_driver.Switch().Frame().
+                    AdminPanel adminPanel = new AdminPanel(_driver);
+                    LoginPage loginPage = new LoginPage(_driver);
+                    loginPage.GoToPage();
 
 
-            loginPage.UserName.Click();
-            //var el = _driver.FindElementById("txtUsername");
+                    //_driver.SwitchTo().Frame("preview-frame");
 
-            loginPage.UserName.SendKeys("opensourcecms");
-            loginPage.Password.SendKeys("opensourcecms");
-            loginPage.Submit.Click();
+                    WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+                    //check if there is any iframe then get that frame and switch on frame using folloiwng code.
+                    var frame = wait.Until(ExpectedConditions.ElementExists(By.Name("preview-frame")));
+                    _driver.SwitchTo().Frame(frame);
 
 
-            //homePage.MyAccount.Click();
-            //var loginPage = new LoginPage(_driver);
-            //loginPage.UserName.SendKeys(_login);
-            //loginPage.Password.SendKeys(_password);
-            //loginPage.Submit.Submit();
-            //Assert.IsTrue(homePage.HeaderText.Displayed);
-            //Assert.AreEqual("Your Account", homePage.HeaderText.Text);
+                    loginPage.UserName.Click();
+                    //var el = _driver.FindElementById("txtUsername");
+
+                    loginPage.UserName.SendKeys("opensourcecms");
+                    loginPage.Password.SendKeys("opensourcecms");
+                    loginPage.Submit.Click();
+
+                    //_driver.SwitchTo().Frame(0);
+                    System.Threading.Thread.Sleep(2000);
+                    var button = _driver.FindElement(By.XPath(@"/html/body/div[5]/ul/li[1]/ul/li[3]/ul/li[1]/a"));
+                    _driver.ExecuteScript("arguments[0].scrollIntoView(true);", button);
+                    wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.XPath(@"/html/body/div[5]/ul/li[1]/ul/li[3]/ul/li[1]/a")));
+
+                    //Klikamy w Menu
+                    Actions actions = new Actions(_driver);
+                    actions.MoveToElement(adminPanel.MenuAdmin).Perform();
+                    actions.MoveToElement(adminPanel.MenuAdminQualification).Perform();
+                    actions.MoveToElement(adminPanel.MenuAdminQualificationSkills).Click().Perform();
+                    
+                    _driver.SwitchTo().Frame(0);
+                    List<Skills> skills = new List<Skills>();
+                    string randomPart = System.Guid.NewGuid().ToString().Substring(0, 4);
+                    skills.Add(new Skills() { Skill = "skill"+randomPart, SkillDescription = "description1" });
+
+                    randomPart = System.Guid.NewGuid().ToString().Substring(0, 4);
+                    skills.Add(new Skills() { Skill = "skill2"+randomPart, SkillDescription = "description2" });
+
+                    foreach (Skills skill in skills)
+                    {
+                        adminPanel.BtnAdd.Click();
+                        adminPanel.InputSkillName.SendKeys(skill.Skill);
+                        adminPanel.InputSkillDescription.SendKeys(skill.SkillDescription);
+                        adminPanel.BtnSave.Click();
+                        if (adminPanel.MessageBallonSuccess.Text != "Successfully Saved")
+                            throw new Exception("Błąd!");
+                    }
+                    //Assert.IsTrue(homePage.HeaderText.Displayed);
+                    //Assert.AreEqual("Your Account", homePage.HeaderText.Text);
+                }
+                catch(Exception ex)
+                {
+                    throw;
+                }
+                finally
+                {
+                    _driver.Quit();
+                }
+            }
+        }
+
+        public class Skills
+        {
+            public string Skill { get; set; }
+            public string SkillDescription { get; set; }
         }
     }
 }
